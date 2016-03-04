9e81c10b48b5ed0553e05573c8a56beb9b9ae810
John Soo <jsoo1@asu.edu>
2016-03-04 15:40:55 -0800

    (feat)User location syncs to mongo

--- server/server.js
+++ server/server.js
@@ -70,8 +70,7 @@ io.on('connection', function(socket){
   console.log('*** Client has Connected');  
   
   socket.on('coords', function syncCoords(data) {
-    console.log(data);
-    socket.emit('coords', data);
+    require('./controllers/userControllers.js').updateLocation(data);
   });
   
   socket.on('disconnect', function(){
